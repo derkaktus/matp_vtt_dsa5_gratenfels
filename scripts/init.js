19e54f76579a1daf11c55b7a4adbe6b9e43bcd94
Christian Mueller <derkaktus@mailbox.org>
2022-08-15 11:01:10 +0200

    zip

--- scripts/init.js
+++ scripts/init.js
@@ -10,14 +10,14 @@
    * Set to the following to disable:
    *   const welcomeJournal = '';
    */
-  const welcomeJournal = 'Startjournal';
+  const welcomeJournal = 'Gratenfels';
   /**
    * additionalJournals will automatically be imported. This is a list of Journals by name that should
    * also be imported.
    * Set to the following to disable:
    *   const additionalJournals = [];
    */
-  const additionalJournals = ["addJournals"];
+  const additionalJournals = [];
   /**
    * additionalMacros will automatically be imported. Each name must match exactly and are case sensitive.
    * Set to the following to disable:
